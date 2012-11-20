be2d8f46024868d3b880fc5ef71be9d90f9d1371
Chris Miller <cmille142@gmail.com>
2012-11-19 23:14:40 -0800

    created alphabet game and game definitions

---
+++ www-dev/js/game/definitions.js
@@ -0,0 +1,14 @@
+/*global define, Media, document, window*/
+
+define(
+  [
+    'game/alphabet/List'
+  ],
+  function (AlphabetSelection) {
+    "use strict";
+
+    return {
+      AlphabetSelection : AlphabetSelection
+    }
+
+  });
